872d4bbca1faef9b61a202aa5d62e5424af46e59
nick arrasate <Narrasate22@icloud.com>
2019-04-12 12:10:16 -0700

    better buttons.

--- assets/subscription-flow-script.js
+++ assets/subscription-flow-script.js
@@ -2,7 +2,7 @@ $('#subscription_start').click(function (e) {
   e.preventDefault()
   $('.takeover_1').show()
 })
-$('#takeover_closer').click(function (e) {
+$('.takeover_closer').click(function (e) {
   e.preventDefault()
   $('.takeover').hide()
 })
