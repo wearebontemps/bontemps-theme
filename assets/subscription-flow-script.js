bd05157ac7b196e820e48d25195494ea19d24b61
nick arrasate <Narrasate22@icloud.com>
2019-04-18 09:42:16 -0700

    refactor vars and set up active switching.

--- assets/subscription-flow-script.js
+++ assets/subscription-flow-script.js
@@ -4,39 +4,39 @@ $(document).on('cart.requestStarted', function (event, cart) {
 $(document).on('cart.ready', function (event, cart) {
   console.log('Cart Ready')
 })
-var product_text_1 = $('#selected_product_1')
-var product_text_2 = $('#selected_product_2')
-var product_text_3 = $('#selected_product_3')
-var product_id_1 = null
-var product_id_2 = null
-var product_id_3 = null
-var product_full_1 = null
-var product_full_2 = null
-var product_full_3 = null
+// var product_text_1 = $('#selected_product_1')
+// var product_text_2 = $('#selected_product_2')
+// var product_text_3 = $('#selected_product_3')
+var productID1 = null
+var productID2 = null
+var productID3 = null
+var productFull1 = null
+var productFull2 = null
+var productFull3 = null
 var subscriptionInterval = null
 function checkFull () {
-  if (product_id_1 != null && product_id_2 != null && product_id_3 != null) {
+  if (productID1 != null && productID2 != null && productID3 != null) {
     $('#bundle-adder-btn').fadeIn(200)
   } else {
     $('#bundle-adder-btn').fadeOut(200)
   }
 }
 function addProduct (id, title, tag) {
-  var title = title;
-  if ($('#selected_product_1').text() == '' ) {
-    $('#selected_product_1').text(title);
-    product_id_1 = id;
-    product_full_1 = tag;
+  var title = title
+  if ($('#selected_product_1').text() == '') {
+    $('#selected_product_1').text(title)
+    productID1 = id
+    productFull1 = tag
     checkFull();
   } else if ($('#selected_product_2').text() == '') {
     $('#selected_product_2').text(title)
-    product_id_2 = id
-    product_full_2 = tag
+    productID2 = id
+    productFull2 = tag
     checkFull()
   } else {
     $('#selected_product_3').text(title)
-    product_id_3 = id
-    product_full_3 = tag
+    productID3 = id
+    productFull3 = tag
     checkFull()
   }
   $('html, body').animate({scrollTop: $('#bundle_anchor').offset().top -380}, 800)
@@ -44,46 +44,46 @@ function addProduct (id, title, tag) {
 function removeProduct (loc) {
   if (loc == '1') {
     $('#selected_product_1').text('')
-    product_id_1 = null
+    productID1 = null
     checkFull()
   } else if (loc == '2') {
     $('#selected_product_2').text('')
-    product_id_2 = null
+    productID2 = null
     checkFull()
   } else if (loc == '3') {
     $('#selected_product_3').text('')
-    product_id_3 = null
+    productID3 = null
     checkFull()
   }
 };
 function bundleOverview() {
-  id_array = []
-  full_size_array = []
-  if (product_id_1 != undefined && product_id_1 != null) {
-    id_array.push(product_id_1)
+  var idArray = []
+  var fullSizeArray = []
+  if (productID1 !== undefined && productID1 != null) {
+    idArray.push(productID1)
   }
-  if (product_id_2 != undefined && product_id_2 != null) {
-    id_array.push(product_id_2)
+  if (productID2 !== undefined && productID2 != null) {
+    idArray.push(productID2)
   }
-  if (product_id_3 != undefined && product_id_3 != null) {
-    id_array.push(product_id_3)
+  if (productID3 !== undefined && productID3 != null) {
+    idArray.push(productID3)
   }
-  if (product_full_1 != undefined && product_full_1 != null) {
-    full_size_array.push(product_full_1)
+  if (productFull1 !== undefined && productFull1 != null) {
+    fullSizeArray.push(productFull1)
   }
-  if (product_full_2 != undefined && product_full_2 != null) {
-    full_size_array.push(product_full_2)
+  if (productFull2 !== undefined && productFull2 != null) {
+    fullSizeArray.push(productFull2)
   }
-  if (product_full_3 != undefined && product_full_3 != null) {
-    full_size_array.push(product_full_3)
+  if (productFull3 !== undefined && productFull3 != null) {
+    fullSizeArray.push(productFull3)
   }
-  console.log('ids:', id_array, 'full-size:', full_size_array);
-if(id_array.length == 3 ) {
+  console.log('ids:', idArray, 'full-size:', fullSizeArray);
+if(idArray.length == 3 ) {
     console.log('inside if', subscriptionInterval)
-    var shipping_interval_frequency = subscriptionInterval.toString()
-    var shipping_interval_unit_type = 'Month'
-    var subscription_id = '203086'
-    async.each(id_array, function(id, next) {
+    var shippingIntervalFrequency = subscriptionInterval.toString()
+    var shippingIntervalUnitType = 'Month'
+    var subscriptionID = '203086'
+    async.each(idArray, function (id, next) {
       CartJS.addItem(id, 1, {
         'Part of a Bundle': '',
         'test': ''
@@ -107,11 +107,11 @@ if(id_array.length == 3 ) {
       }
     });
     //  run full size
-    async.each(full_size_array, function (id, next) {
+    async.each(fullSizeArray, function (id, next) {
       CartJS.addItem(id, 1, {
-        'shipping_interval_frequency': shipping_interval_frequency,
-        'shipping_interval_unit_type': shipping_interval_unit_type,
-        'subscription_id': subscription_id
+        'shipping_interval_frequency': shippingIntervalFrequency,
+        'shipping_interval_unit_type': shippingIntervalUnitType,
+        'subscription_id': subscriptionID
       }, {
         'complete': function () {
           console.log('2 complete!')
@@ -137,35 +137,34 @@ if(id_array.length == 3 ) {
 };
 }
 function selectInterval (interval) {
+  $('.flow-card').removeClass('active')
   if (interval == 1) {
     subscriptionInterval = 1
-    console.log(subscriptionInterval)
+    $('.interval_selector_1').addClass('active')
   } else if (interval == 2) {
     subscriptionInterval = 2
-    console.log(subscriptionInterval)
+    $('.interval_selector_2').addClass('active')
   } else if (interval == 3) {
     subscriptionInterval = 3
-    console.log(subscriptionInterval)
+    $('.interval_selector_3').addClass('active')
   }
 }
 function nextView(pos) {
   if (pos < 3 && pos > 0) {
-    index = pos + 1;
+    var index = pos + 1;
     $('.takeover_' + pos).fadeOut(0)
     $('.takeover_' + index).fadeIn(300)
     $('html, body').animate({ scrollTop: 0 }, 'slow')
     if (pos == 2 ) {
       bundleOverview()
     }
-  } else {
-      return
   }
 }
 function prevView (pos) {
   if (pos < 4 && pos > 0) {
-    index = pos - 1;
-    $('.takeover_' + pos).fadeOut(0);
-    $('.takeover_' + index).fadeIn(300);
+    var index = pos - 1
+    $('.takeover_' + pos).fadeOut(0)
+    $('.takeover_' + index).fadeIn(300)
     $('html, body').animate({ scrollTop: 0 }, 'slow')
   }
 }
@@ -174,23 +173,23 @@ var buildOverview = function (cart, loc) {
   var loc = loc
   if (loc == 1) {
     var domSelector = $('#sub-flow-cart-upper')
-    var useArray = id_array
+    useArray = idArray
   } else if (loc == 2 ) {
     var domSelector = $('#sub-flow-cart-lower')
-    var useArray = full_size_array;
+    useArray = fullSizeArray;
     $('.interval_target').text(subscriptionInterval.toString())
   }
   domSelector.html('')
   console.log('cart:', cart)
   console.log('build_samples ids:', useArray)
-  for (j = 0; j < useArray.length; j++) {
-    for (i = 0; i < cart.items.length ; i++) {
+  for (j=0; j<useArray.length; j++) {
+    for (i=0; i<cart.items.length; i++) {
       if (cart.items[i].id == useArray[j]) {
-        item = cart.items[i]
+        var item = cart.items[i]
         var str = item.price.toString()
-        var priceFormatted = '$'+str.substring(0,str.length-2)+"."+str.substring(str.length-2)
+        var priceFormatted = '$' + str.substring(0, str.length - 2) + '.' + str.substring(str.length - 2)
         domSelector.append(
-          '<div class="row" style="margin: .25em auto;">'+
+          '<div class="row" style="margin: .25em auto;">' +
           '<div class="col-4 d-flex align-items-center" data-label="{{ "customer.order.product" | t }}">' +
           '<img src="' + item.image + '" alt="' + item.title + '"/>' +
           '</div>' +
