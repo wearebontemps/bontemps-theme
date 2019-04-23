4017e59f7dd95d66e89431b51b1c82cca2bd90ef
nick arrasate <Narrasate22@icloud.com>
2019-04-23 10:03:41 -0700

    better redirect.

--- assets/subscription-flow-script.js
+++ assets/subscription-flow-script.js
@@ -244,11 +244,23 @@ var subscriptionCart = function (loc) {
     buildOverview(cart, loc)
   })
 }
+
+function reChargeProcessCart () {
+  function get_cookie (name) { return( document.cookie.match('(^|; )' + name + '=([^;]*)') || 0)[2] }
+  do {
+    token = get_cookie('cart')
+  }
+  while (token == undefined)
+	var myshopify_domain='{{ shop.permanent_domain }}'
+	try { var ga_linker = ga.getAll()[0].get('linkerParam') } catch(err) { var ga_linker ='' }
+	var customer_param = '{% if customer %}customer_id={{customer.id}}&customer_email={{customer.email}}{% endif %}'
+	checkout_url= "https://checkout.rechargeapps.com/r/checkout?myshopify_domain="+myshopify_domain+"&cart_token="+token+"&"+ga_linker+"&"+customer_param;
+}
 var checkoutRedirect = function () {
-  // for (i = 0; i < fullSizeArray.length; i++) {
-  //   CartJS.removeItemById(fullSizeArray[i])
-  // }
-  window.location.replace('/checkout')
+  for (i = 0; i < fullSizeArray.length; i++) {
+    CartJS.removeItemById(fullSizeArray[i])
+  }
+  reChargeProcessCart()
 }
 $('#subscription_start').click(function (e) {
   e.preventDefault()
