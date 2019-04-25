04dacd17237852e3e812c190ac06a50453cd9a5c
nick arrasate <Narrasate22@icloud.com>
2019-04-25 16:05:34 -0700

    fix mobile starter button

--- assets/subscription-flow-script.js
+++ assets/subscription-flow-script.js
@@ -209,6 +209,10 @@ $('#subscription_start').click(function (e) {
   e.preventDefault()
   $('.takeover_1').fadeIn(300)
 })
+$('#subscription_start_2').click(function (e) {
+  e.preventDefault()
+  $('.takeover_1').fadeIn(300)
+})
 $('.takeover_closer').click(function (e) {
   e.preventDefault()
   $('.takeover').fadeOut(300)
