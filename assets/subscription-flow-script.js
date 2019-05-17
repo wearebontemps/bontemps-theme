33c6cbbe5ab3d228c07ab59fb68a0f698fc7d2a0
nick arrasate <Narrasate22@icloud.com>
2019-05-17 10:43:06 -0700

    switch to update.

--- assets/subscription-flow-script.js
+++ assets/subscription-flow-script.js
@@ -59,7 +59,7 @@ function bundleOverview () {
     var shippingIntervalUnitType = 'Month'
     var subscriptionID = '206077'
     async.each(idArray, function (id, next) {
-      CartJS.addItem(id, 1, {
+      CartJS.updateItem(id, 1, {
         'shipping_interval_frequency': shippingIntervalFrequency,
         'shipping_interval_unit_type': shippingIntervalUnitType,
         'subscription_id': subscriptionID,
